362697f23465e3f1ed6a77ff858a52413f4d787d
alaazetauny <alaachz.83@gmail.com>
2026-08-11 13:41:46 +0200

    skills component

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { BsInfoCircleFill } from "react-icons/bs";
+import { skillsData } from "./utils";
+import { Line } from "rc-progress";
+import "./styles.scss";
 
 const Skills = () => {
   return (
@@ -9,6 +12,29 @@ const Skills = () => {
         headerText="My Skills"
         icon={<BsInfoCircleFill size={40} />}
       />
+      <div className="skills__content-wrapper">
+        {skillsData.map((item, index) => (
+          <div key={index} className="skills__content-wrapper__inner-content">
+            <h3 className="skills__content-wrapper__inner-content__category-text">
+              {item.label}
+            </h3>
+            <div>
+              {item.data.map((skillsItem, j) => (
+                <div className="progressbar-wrapper" key={j}>
+                  <p>{skillsItem.skillName}</p>
+                  <Line
+                    percent={skillsItem.percentage}
+                    strokeWidth="2"
+                    strokeColor="var(--yellow-theme-main-color)"
+                    trailWidth="2"
+                    strokeLinecap="square"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,57 @@
+export const skillsData = [
+  {
+    label: "FRONT END",
+    data: [
+      {
+        skillName: "HTML",
+        percentage: "90",
+      },
+      {
+        skillName: "CSS",
+        percentage: "50",
+      },
+      {
+        skillName: "JAVASCRIPT",
+        percentage: "80",
+      },
+      {
+        skillName: "TYPESCRIPT",
+        percentage: "70",
+      },
+      {
+        skillName: "REACTJS",
+        percentage: "70",
+      },
+    ],
+  },
+  {
+    label: "BACK END",
+    data: [
+      {
+        skillName: "JAVA",
+        percentage: "90",
+      },
+      {
+        skillName: "PYTHON",
+        percentage: "50",
+      },
+      {
+        skillName: "SPRINGBOOT",
+        percentage: "80",
+      },
+    ],
+  },
+  {
+    label: "DATABASES",
+    data: [
+      {
+        skillName: "SQL",
+        percentage: "90",
+      },
+      {
+        skillName: "MYSQL",
+        percentage: "90",
+      },
+    ],
+  },
+];
