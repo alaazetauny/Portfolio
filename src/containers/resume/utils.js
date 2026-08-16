eafc07d9d6177d079c996131d43292c4122a298a
alaazetauny <alaachz.83@gmail.com>
2026-08-16 11:02:03 +0200

    resume component.

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,100 @@
+export const data = {
+  experience: [
+    {
+      title: "Operations Manager",
+      nameOfCompany: "ChyabElektro",
+      description: `Programming and commissioning of KNX systems, Planning and implementation of smart home solutions, Technical coordination and organization of operational processes`,
+    },
+    {
+      title: "Web Developer",
+      nameOfCompany: "Werum Software & Systems AG",
+      description:
+        "Backend implementation of a web editor for DSL specifications, including REST services and persistence layer, Frontend implementation of a web editor for DSL specifications with interactive syntax highlighting, including user interfaces and layouts, Software testing, Analysis and resolution of software issues",
+    },
+    {
+      title: "Working Student",
+      nameOfCompany: "Werum Software & Systems AG",
+      description:
+        "Contributed to backend and frontend implementations, Supported the development and maintenance of software solutions, Performed software testing and error analysis",
+    },
+    {
+      title: "Subject Tutor",
+      nameOfCompany: "HAW Hamburg",
+      description:
+        "Conducted tutorials in Discrete Mathematics, Explained mathematical fundamentals and practice exercises, Supported students in exam preparation",
+    },
+  ],
+  education: [
+    {
+      title: "Bachelor of Science, HAW Hamburg",
+      nameOfCompany: "Applied Computer Science",
+      description: "Grade: 1.93",
+    },
+    {
+      title: "Diploma Engineer, Damascus University – Damascus, Syria",
+      nameOfCompany: "Electrical Engineering and Telecommunications",
+      description: "Grade: 2.7",
+    },
+    {
+      title: "Vocational Training in Telecommunications",
+      nameOfCompany: "Damascus, Syria",
+      description: "Grade: 1.9",
+    },
+    {
+      title: "Scientific High School Diploma",
+      nameOfCompany: "High School in Damascus, Syria",
+      description: "Grade: 1.7",
+    },
+  ],
+};
+/* {
+  experience: [
+    {
+      title: " Betriebsleiterin ",
+      nameOfCompanie: "ChyabElektro",
+      description:
+        "	• Programmierung und Inbetriebnahme von KNX-Systemen '\n' • Planung und Umsetzung von Smart-Home-Lösungen '\n' • Technische Koordination und Organisation betrieblicher Abläufe",
+    },
+    {
+      title: " Webentwicklerin ",
+      nameOfCompanie: "Werum Software & Systems AG",
+      description:
+        "• Backend-Implementierung eines Web-Editors für DSL-Spezifikationen (Umsetzung von REST-Services inkl. Persistenzschicht) '\n' •	Frontend- Implementierung eines Web-Editors für DSL-Spezifikationen mit interaktivem Syntax-Highlighten (Umsetzung von Oberflächen und Layouts) '\n' 	•	Durchführung von Softwaretests'\n' •	Analyse und Behebung von Softwarefehlern",
+    },
+    {
+      title: "Werkstudentin",
+      nameOfCompanie: "Werum Software & Systems AG",
+      description:
+        "	•	Mitarbeit bei Backend- und Frontend-Implementierungen \n •	Unterstützung bei der Entwicklung und Wartung von Softwarelösungen '\n' •	Durchführung von Tests sowie Fehleranalyse",
+    },
+    {
+      title: "Fachtutorin ",
+      nameOfCompanie: "HAW Hamburg",
+      description:
+        "	•	Durchführung eines Tutoriums im Fach Diskrete Mathematik '\n'•	Erläuterung mathematischer Grundlagen und Übungsaufgaben '\n' •	Prüfungsvorbereitung der Studierenden ",
+    },
+  ],
+  education: [
+    {
+      title: " Bachelor of Science an der HAW Hamburg ",
+      nameOfCompanie: "Angewandte Informatik",
+      description: "	1,93 gut",
+    },
+    {
+      title:
+        " Diplomingenieurin an  der Universität Damaskus - Damaskus, Syrien ",
+      nameOfCompanie: "Diplomstudium Elektrotechnik und Telekommunikation ",
+      description: "2,7",
+    },
+    {
+      title: "Ausbildung im Bereich Telekommunikation",
+      nameOfCompanie: "Damaskus, Syrien",
+      description: "1,9",
+    },
+    {
+      title: "Wissenschaftliches Abitur  ",
+      nameOfCompanie: "Gymnasium in Damaskus, Syrien",
+      description: "1,7",
+    },
+  ],
+}; */
